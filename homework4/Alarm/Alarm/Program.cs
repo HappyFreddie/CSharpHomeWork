64987334a757b51ceea17099c28f6fc48e3743cb
HappyFreddie <51391384+HappyFreddie@users.noreply.github.com>
2020-03-15 16:32:04 +0800

    week 4

---
+++ homework4/Alarm/Alarm/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alarm
+{
+    public delegate void AlarmHandler(object sender,AlarmEventArgs args);
+
+    // 闹钟的参数
+    public class AlarmEventArgs : EventArgs
+    {
+        public int Hour { get; }
+        public int Minute { get; }
+        public int Second { get; }
+        public string Words { get; }
+
+        public AlarmEventArgs(int hour, int minute, int second, string words)
+        {
+            this.Hour = hour; this.Minute = minute; this.Second = second; this.Words = words;
+        }
+    }
+
+    // 闹钟具体功能
+    public class Bell
+    {
+        int Hour = 0, Minute = 0, Second = 0;
+
+        public event AlarmHandler Tick;
+        public event AlarmHandler Alarm;
+
+        public void StartBell()
+        {
+            while(!(DateTime.Now.Hour == Hour && DateTime.Now.Minute == Minute && DateTime.Now.Second == Second))
+            { }
+            AlarmEventArgs alarmEventArgs1 = new AlarmEventArgs(Hour,Minute,Second,"时间到了!!!");
+            Alarm(this, alarmEventArgs1);
+        }
+
+        public void _Tick()
+        {
+            while(!(DateTime.Now.Hour == Hour && DateTime.Now.Minute == Minute && DateTime.Now.Second == Second))
+            {
+                    AlarmEventArgs alarmEventArgs2 = new AlarmEventArgs(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second, "滴答...");
+                    Tick(this, alarmEventArgs2);
+            }
+
+        }
+
+        public bool SetTime(int h,int m, int s)
+        {
+            if (h > 24 || m > 60 || s > 60)
+                return false;
+            Hour = h; Minute = m; Second = s;
+            return true;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bell bell = new Bell();
+            bell.SetTime(16, 27, 30);
+            bell.Tick += new AlarmHandler(AlarmTrigger);
+            bell.Alarm += new AlarmHandler(AlarmTrigger);
+            bell._Tick();
+            bell.StartBell();
+        }
+
+        private static void AlarmTrigger(object sender,AlarmEventArgs a)
+        {
+            string message = string.Format("{0}时{1}分{2}秒,{3}", a.Hour, a.Minute, a.Second, a.Words);
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ homework4/GenericApplication/GenericApplication/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+                head = tail = n;
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while(temp.Next != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+            action(temp.Data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> genericList = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+                genericList.Add(x);
+            // 显示元素
+            Console.WriteLine("元素:");
+            genericList.Foreach(x => Console.Write(x));
+            int max = 0, min = 0;
+            Console.WriteLine("最大值:");
+            genericList.Foreach(x => { if (max < x) max = x; });
+            Console.WriteLine(max);
+            Console.WriteLine("最小值:");
+            genericList.Foreach(x => { if (min > x) min = x; });
+            Console.WriteLine(min);
+            int sum = 0;
+            Console.WriteLine("求和:");
+            genericList.Foreach(x => sum += x);
+            Console.WriteLine(sum);
+        }
+    }
+}
